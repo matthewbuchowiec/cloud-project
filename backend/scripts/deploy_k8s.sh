32ab8c65d5085ef799c1683deb0e48c63ff43a9d
salomondush <63796975+salomondush@users.noreply.github.com>
2024-04-02 03:44:10 -0500

    Update replicas in fastapi-deployment.yaml and deploy_k8s.sh

--- backend/scripts/deploy_k8s.sh
+++ backend/scripts/deploy_k8s.sh
@@ -4,6 +4,6 @@ echo "Applying the deployments and services"
 
 kubectl apply -f ../k8s/fastapi-deployment.yaml
 kubectl apply -f ../k8s/fastapi-service.yaml
-kubectl autoscale deployment fastapi-deployment --cpu-percent=50 --min=1 --max=4
+kubectl autoscale deployment fastapi-deployment --cpu-percent=50 --min=2 --max=4
 
 kubectl apply -f ../k8s/components.yaml
