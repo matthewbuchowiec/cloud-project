0aa6aa7d895b7187866ccab6276cf89bd1e02576
Olivia Liu <xiaohan.liu.1@vanderbilt.edu>
2024-04-07 19:40:58 -0500

    added basic keyword analysis, refactored db

---
+++ backend/database.py
@@ -0,0 +1,21 @@
+import boto3
+from datetime import datetime
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+
+table = dynamodb.Table('news_table_us-east-2')
+
+def get_all_news():
+    response = table.scan() 
+    data = response['Items']
+    articles = [item['articles'] for item in data]
+    return articles
+
+def get_news_by_date(): 
+    response = table.query(
+    IndexName="date_created-index",
+    KeyConditionExpression=Key('date_created').eq(
+        datetime.today().strftime('%Y-%m-%d')))
+
+    return response["Items"]
