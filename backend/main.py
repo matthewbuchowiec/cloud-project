2e60a67e7edcbd761fed330ed0a5c7eab6644ece
salomondush <63796975+salomondush@users.noreply.github.com>
2024-04-16 14:50:56 -0500

    Refactor main.py to fix cache initialization and remove commented code

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
 import keywords
 import database
+
 
 class Data(BaseModel):
     key: str
@@ -22,11 +22,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 scheduler = AsyncIOScheduler()
-cache = {} 
+cache = {}
+
 
 @app.get("/")
 def default():
     return "Fast API News Server"
+
 
 @app.get("/test/")
 async def load_test():
@@ -51,17 +53,12 @@ async def get_news():
     """
     return database.get_news_by_date()
 
+
 def fetch_and_cache_data():
     articles = database.get_all_news()
     analysis = keywords.get_analysis(articles)
     cache["keywords_analysis"] = analysis
 
-# @app.on_event("startup")
-# async def start_scheduler():
-#     # Schedule the `fetch_and_cache_data` to run every day at 7:00 AM
-#     scheduler.add_job(fetch_and_cache_data, trigger=CronTrigger(hour=7, minute=0))
-#     scheduler.start()
-#     fetch_and_cache_data()
 
 @app.get("/keywords/")
 async def get_keywords_analysis():
