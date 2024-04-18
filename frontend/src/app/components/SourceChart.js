500257afcb3263ae734a86a83c9db3367b498cf1
matthewbuchowiec <matthew.m.buchowiec@vanderbilt.edum>
2024-04-17 21:30:16 -0500

    changed pie chart styles

--- frontend/src/app/components/SourceChart.js
+++ frontend/src/app/components/SourceChart.js
@@ -3,47 +3,59 @@ import React, { useState, useEffect, forwardRef } from "react";
 import { Doughnut } from "react-chartjs-2";
 import "chart.js/auto";
 
-const SourceChart = forwardRef(
-  ({ category, setSelectedSource, counts }, ref) => {
-    const [renderData, setRenderData] = useState();
-    useEffect(() => {
-      const entries = Object.entries(counts);
-      entries.sort((a, b) => b[1] - a[1]);
-      const sortedEntries = entries.slice(0, 7);
-      const sortedData = Object.fromEntries(sortedEntries);
-      const data = {
-        labels: Object.keys(sortedData),
-        datasets: [
-          {
-            label: "Sources",
-            data: Object.values(sortedData),
-            borderWidth: 1,
-          },
-        ],
-      };
-      setRenderData(data);
-    }, [category, counts]);
+const SourceChart = forwardRef(({ category, setSelectedSource, counts }, ref) => {
+  const [renderData, setRenderData] = useState();
 
-    const handleClick = (_event, elements) => {
-      if (elements.length !== 0) {
-        const elementIndex = elements[0].index;
-        const label = renderData.labels[elementIndex];
-        setSelectedSource(label);
-      }
+  useEffect(() => {
+    const entries = Object.entries(counts);
+    entries.sort((a, b) => b[1] - a[1]);
+    const sortedEntries = entries.slice(0, 7);
+    const sortedData = Object.fromEntries(sortedEntries);
+
+    const colors = [
+      "#FF6384",
+      "#36A2EB",
+      "#FFCE56",
+      "#4BC0C0",
+      "#9966FF",
+      "#FF9F40",
+      "#E7E9ED",
+    ];
+
+    const data = {
+      labels: Object.keys(sortedData),
+      datasets: [
+        {
+          label: "Sources",
+          data: Object.values(sortedData),
+          backgroundColor: colors,
+          borderWidth: 1,
+        },
+      ],
     };
 
-    const options = {
-      maintainAspectRatio: false,
-      onClick: handleClick,
-    };
+    setRenderData(data);
+  }, [category, counts]);
 
-    return (
-      <div style={{ width: "300px", height: "300px" }} ref={ref}>
-        {renderData && <Doughnut data={renderData} options={options} />}
-      </div>
-    );
-  }
-);
+  const handleClick = (_event, elements) => {
+    if (elements.length !== 0) {
+      const elementIndex = elements[0].index;
+      const label = renderData.labels[elementIndex];
+      setSelectedSource(label);
+    }
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    onClick: handleClick,
+  };
+
+  return (
+    <div style={{ width: "300px", height: "300px" }} ref={ref}>
+      {renderData && <Doughnut data={renderData} options={options} />}
+    </div>
+  );
+});
 
 SourceChart.displayName = "SourceChart";
 
